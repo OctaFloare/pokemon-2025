b8f62252284bbe5c0e7985a366d91715985e3993
Marius Achim <mariusachim2004@gmail.com>
2025-07-06 19:04:21 +0300

    tema

--- src/componets/navigation/index.jsx
+++ src/componets/navigation/index.jsx
@@ -1,5 +1,5 @@
 export const Navigation = (props) => {
-    return <div className="flex gap-10 bg-[#E5E4E2] fixed left-0 top-0 w-auto justify-center">
+    return <div className="flex gap-10 bg-[#E5E4E2] fixed left-0 top-0 w-full justify-center">
         <a href="/">Home</a>
         <a href="/pokemons">Pokemons</a>
         <div>{props.test}</div>
