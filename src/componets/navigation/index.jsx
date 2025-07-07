2e37ddc755e43de8f7f3786b7085e52e3309c2aa
Nicu-Stroia <nicu.stroia04@gmail.com>
2025-07-07 13:53:37 +0300

    Remove node_modules and add .gitignore

--- src/componets/navigation/index.jsx
+++ src/componets/navigation/index.jsx
@@ -1,6 +1,11 @@
 export const Navigation = () => {
     return <div className="flex gap-5 bg-[#E5E4E2] fixed left-0 top-0 w-full justify-center">
         <a href="/">Home</a>
+<<<<<<< HEAD
         <a href="/pokemons">Pokemons</a>
+=======
+        <a href="pokemons">Pokemons</a>
+        <div>{props.test}</div>
+>>>>>>> 37e9d8d (Remove node_modules and add .gitignore)
     </div>
 }
