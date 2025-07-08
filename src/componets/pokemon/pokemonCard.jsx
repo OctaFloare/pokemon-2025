b68f6bdf0b917ea95310da9612e3e6d3bee2ecf2
4ndreiOp <andrei.oprescu.2404@gmail.com>
2025-07-08 11:22:15 +0300

    Refactor Navigation to absolute and PokemonCard components to flex-col for improved layout and styling

--- src/componets/pokemon/pokemonCard.jsx
+++ src/componets/pokemon/pokemonCard.jsx
@@ -3,8 +3,8 @@ import { Moves } from "../moves";
 export const PokemonCard = ({ pokemon }) => {
     const { name, weight, sprites, types } = pokemon;
 
-    return <div className="flex gap-2 justify-center">
-        <img src={sprites.front_default} alt={name} className="w-[300px] h-[300px]"/>
+    return <div className="flex flex-col gap-2 justify-center">
+        <img src={sprites.front_default} alt={name} className="w-[300px] h-[300px] flex self-center"/>
         <div className="flex flex-col justify-center text-2xl">
             <div>Name: {name.charAt(0).toUpperCase() + name.slice(1)}</div>
             <div>Weight: {weight}</div>
